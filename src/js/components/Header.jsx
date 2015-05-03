8f750b9efea1996b8a5f3cbf26463e396577cf01
Matt Aitchison <Matt@lanciv.com>
2015-05-03 15:25:56 -0500

    Header now works for mobile

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import Logo from '../atoms/Logo';
-import Router, {Link} from 'react-router';
-import {Nav, DropdownButton} from 'react-bootstrap';
+
+import Router from 'react-router';
+import {Navbar, Nav, DropdownButton, CollapsibleNav} from 'react-bootstrap';
 import {NavItemLink} from 'react-router-bootstrap';
 
 import api from '../api/api';
 import auth from '../api/auth';
 
-
-let menuItems = [
-  {route: 'dashboard', text: 'Dashboard'},
-  {route: 'class', text: 'Classes'},
-  {route: 'people', text: 'People'},
-  {route: 'users', text: 'Users'},
-  {route: 'setup', text: 'App Settup'}
-];
-
 const HeaderNav = React.createClass({
   mixins: [Router.Navigation, Router.State],
   userDisplayName(){
-    return this.state.person.firstName + " " + this.state.person.lastName;
+    return this.state.person.firstName + ' ' + this.state.person.lastName;
   },
   componentWillMount(){
     const personId = auth.currentUser().personId;
@@ -36,16 +27,12 @@ const HeaderNav = React.createClass({
   getInitialState(){
     return {
       person: {},
-      selectedIndex: null
-    }
+    };
   },
   render() {
     return (
-      <nav className="app-nav">
-        <div className="container">
-          <div className="navbar-header">
-            <span className="navbar-brand">GoGradeMe</span>
-          </div>
+      <Navbar className="app-nav" brand="GoGradeMe" toggleNavKey={0}>
+        <CollapsibleNav eventKey={0}>
           <Nav navbar>
             <NavItemLink to="dashboard">
               Dashboard
@@ -70,77 +57,10 @@ const HeaderNav = React.createClass({
               </NavItemLink>
             </DropdownButton>
           </Nav>
-        </div>
-      </nav>
+        </CollapsibleNav>
+      </Navbar>
     );
-  },
-  _onMenuIconButtonTouchTap() {
-    this.refs.leftNav.toggle();
-  },
-  _getSelectedIndex: function() {
-    // var currentItem;
-    //
-    // for (var i = menuItems.length - 1; i >= 0; i--) {
-    //   currentItem = menuItems[i];
-    //   if (currentItem.route && this.context.router.isActive(currentItem.route)) return i;
-    // };
-  },
-
-  _onLeftNavChange: function(e, key, payload) {
-    this.context.router.transitionTo(payload.route);
-  },
-
-  _onHeaderClick: function() {
-    this.context.router.transitionTo('root');
-    this.refs.leftNav.close();
   }
 });
-//
-// <AppBar
-//   className="mui-dark-theme"
-//   title="GoGradeMe"
-//   zDepth={0}
-//   onMenuIconButtonTouchTap={this._onMenuIconButtonTouchTap}
-//   />
-// <LeftNav
-//   ref="leftNav"
-//   docked={false}
-//   isInitiallyOpen={false}
-//   menuItems={menuItems}
-//   header={header}
-//   selectedIndex={this._getSelectedIndex()}
-//   onChange={this._onLeftNavChange}
-//   />
-
-// <div className="ui fixed blue inverted main menu">
-//   <div className="container">
-//     <Link className="item" to="dashboard">
-//       <i className="dashboard icon" />
-//       Dashboard
-//     </Link>
-//     <Link className="item" to="class">
-//       Classes
-//     </Link>
-//     <Link className="item" to="people">
-//       <i className="users icon"/>
-//       People
-//     </Link>
-//     <Link className="item" to="users">
-//       <i className="users icon"/>
-//       Users
-//     </Link>
-//     <Link className="item" to="setup">
-//       App Setup
-//     </Link>
-//     <div className="right menu">
-//       <div className="item">
-//         {this.userDisplayName()}
-//       </div>
-//       <Link className="item" to="logout">
-//         Logout
-//       </Link>
-//     </div>
-//   </div>
-// </div>
 
 module.exports = HeaderNav;
