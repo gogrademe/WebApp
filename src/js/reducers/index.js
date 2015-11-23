cb7064dbf3138d897dc8120b6935ddce270ce48d
Matt Aitchison <Matt@lanciv.com>
2015-11-22 23:46:56 -0600

    added attempts resource

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -4,7 +4,7 @@ import paginate from './paginate';
 import { combineReducers } from 'redux';
 
 // Updates an entity cache in response to any action with response.entities.
-function entities(state = { users: {}, repos: {}, assignments:{}, groups:{} }, action) {
+function entities(state = { users: {}, repos: {}, assignments:{}, groups:{}, attempts:{} }, action) {
   if (action.response && action.response.entities) {
     return merge({}, state, action.response.entities);
   }
