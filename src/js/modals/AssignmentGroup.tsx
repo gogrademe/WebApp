a90f85bbe8a8ebee3ae7af54caad3a490e833251
Matt Aitchison <matta@clearbit.com>
2019-11-12 14:41:51 -0600

    create assignmentGroups with a gql mutation

--- src/js/modals/AssignmentGroup.tsx
+++ src/js/modals/AssignmentGroup.tsx
@@ -1,59 +1,55 @@
-import * as _ from "lodash";
-import * as React from "react";
-import { connect } from "react-redux";
-import { Field, reduxForm } from "redux-form";
+import React, { useState, useCallback } from "react";
 import { Form } from "semantic-ui-react";
-import api from "../api/api";
+
 import ModalForm from "../components/ModalForm";
+
+import { gql } from "apollo-boost";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { useInput } from "../hooks/input-hook";
 
 interface AssignmentGroupProps {
   courseId: number;
   termId: number;
-  groupId: number;
+  groupId?: number;
 }
 
-export class AssignmentGroup extends React.Component<AssignmentGroupProps, any> {
-  onSubmit = model => {
-    const { courseId, termId, groupId } = this.props;
-    model = { ...model, courseId: Number(courseId), termId: Number(termId), weight: Number(model.weight) };
-    // model.weight = Number(model.weight / 100);
-
-    if (groupId) {
-      model.groupId = groupId;
-      return api.group.update(model.groupId, model);
-    } else {
-      return api.group.create(model);
+const ASSIGNMENT_GROUP = gql`
+  query AssignmentGroup($groupId: ID!) {
+    assignmentGroup(groupId: $groupId) {
+      groupId
+      name
     }
-  };
-  render() {
-    // const weight = Number(this.state.group.weight * 100) || '';
-    return (
-      <ModalForm {...this.props} title="Assignment Group" onSubmitAsync={this.onSubmit}>
-        <Field label="Name" name="name" placeholder="Name" component={Form.Input} />
-        <Field
-          label="Weight"
-          name="weight"
-          validations="isWeight"
-          validationError="must be between .5% and 100%"
-          component={Form.Input}
-        />
-      </ModalForm>
-    );
   }
-}
+`;
 
-const mapStateToProps = (state, ownProps) => {
-  console.log(state);
-  return {
-    initialValues: {
-      ...ownProps.initialValues,
-      ..._.get(state.entities.groups, ownProps.groupId, {})
+const CREATE_ASSIGNMENT_GROUP = gql`
+  mutation CreateAssignmentGroup($courseId: ID!, $termId: ID!, $name: String!, $weight: Float!) {
+    createAssignmentGroup(input: { name: $name, weight: $weight, courseId: $courseId, termId: $termId }) {
+      groupId
+      name
+    }
+  }
+`;
+
+export default function AssignmentGroup({ groupId, courseId, termId, ...rest }: AssignmentGroupProps) {
+  // const { loading, error, data } = useQuery(ASSIGNMENT_GROUP, {
+  //   variables: { groupId: groupId }
+  // });
+
+  const [create] = useMutation(CREATE_ASSIGNMENT_GROUP);
+  const { value: name, bind: bindName } = useInput("");
+  const { value: weight, bind: bindWeight } = useInput("");
+
+  const onSubmit = () => {
+    if (groupId == undefined) {
+      create({ variables: { name: name, weight: weight, courseId: courseId, termId: termId } });
     }
   };
-};
 
-export default connect(mapStateToProps)(
-  reduxForm({
-    form: "assignmentGroup" // a unique identifier for this form
-  })(AssignmentGroup)
-);
+  return (
+    <ModalForm {...rest} title="Assignment Group" onSubmitAsync={onSubmit} handleSubmit={onSubmit}>
+      <Form.Input name="name" placeholder="Name" label="Name" {...bindName} />
+      <Form.Input name="weight" label="Weight" {...bindWeight} />
+    </ModalForm>
+  );
+}
